e72755d24fb81b3946fee80503b9f096ed1ed1f1
Lawrence <lawrencestewart1@gmail.com>
2019-11-06 17:37:13 -0800

    after build

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
+import pathlib2
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# The directory containing this file
+HERE = pathlib2.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="pyPelias",
     version="0.1.0",
     author="Lawrence Stewart",
     author_email="lawrence@lawrencestewart.ca",
-    description="A simple python wrapper for the Pelias API for geocoding and reverse geocoding.",
-    long_description=long_description,
+    description="A simple python wrapper for the Pelias API for geocoding and reverse geocoding",
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/getorca",
+    url="https://github.com/getorca/pyPelias",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
